f1231642e26f57e4d0c83d6fbe904414eeecd8db
evertonhf <evertonhf@gmail.com>
2024-11-25 21:33:28 -0300

    Ajuste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CepService } from './cep.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'person-address';
+  
+  nome: string = '';
+  email: string = '';
+  cep: string = '';
+  address: any;
+  showAlert: boolean = false;
+
+  constructor(private cepService: CepService) {}
+
+  searchCep() {
+    if (!this.cep) {
+      this.showAlert = true;
+      return;
+    }
+    this.showAlert = false;
+    this.cepService.getCepData(this.cep).subscribe(
+      (data: any) => {
+        this.address = data;
+      },
+      (error) => {
+        console.error('Erro ao buscar o CEP', error);
+      }
+    );
+  }
 }
